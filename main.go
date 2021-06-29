153aee7ae5e76f63f93150c548cc09b87bbbd19e
akrindev <akrinmin@gmail.com>
2021-06-29 20:46:51 +0700

    print

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hellow World! \n")
+	fmt.Println("Hellow from go with new line")
+
+	fmt.Printf("The color of %s is %s \n", "apple", "red")
+
+}
